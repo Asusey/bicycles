4aa8dcd068be6d606f170d36ee938bcd20552802
Anna Tolstikova <39711434+Asusey@users.noreply.github.com>
2021-09-14 23:46:18 +0300

    Чистка кода, исправления по критериям

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,15 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var pageHeader = document.querySelector('.main-nav');
+var headerToggle = document.querySelector('.main-nav__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+pageHeader.classList.remove('main-nav--nojs');
 
 headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+  if (pageHeader.classList.contains('main-nav--closed')) {
+    pageHeader.classList.remove('main-nav--closed');
+    pageHeader.classList.add('main-nav--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    pageHeader.classList.add('main-nav--closed');
+    pageHeader.classList.remove('main-nav--opened');
   }
 });
