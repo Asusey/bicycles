f0a9e667bd0431c0dc684674677a67fec251a222
Anna Tolstikova <39711434+Asusey@users.noreply.github.com>
2021-09-20 16:54:34 +0300

    eslint

--- build/js/script.js
+++ build/js/script.js
@@ -1,29 +1,29 @@
-//"use strict";
+'use strict';
 
-;(function () {
-  var pageHeader = document.querySelector('.main-nav')
-  var headerToggle = document.querySelector('.main-nav__toggle')
-  var headerLinks = document.querySelectorAll('.main-nav__link')
+(function () {
+  var pageHeader = document.querySelector('.main-nav');
+  var headerToggle = document.querySelector('.main-nav__toggle');
+  var headerLinks = document.querySelectorAll('.main-nav__link');
 
-  pageHeader.classList.remove('main-nav--nojs')
+  pageHeader.classList.remove('main-nav--nojs');
 
   headerToggle.addEventListener('click', function () {
     if (pageHeader.classList.contains('main-nav--closed')) {
-      pageHeader.classList.remove('main-nav--closed')
-      pageHeader.classList.add('main-nav--opened')
-      document.getElementsByTagName('body')[0].style.overflow = 'hidden'
+      pageHeader.classList.remove('main-nav--closed');
+      pageHeader.classList.add('main-nav--opened');
+      document.getElementsByTagName('body')[0].style.overflow = 'hidden';
     } else {
-      pageHeader.classList.add('main-nav--closed')
-      pageHeader.classList.remove('main-nav--opened')
-      document.getElementsByTagName('body')[0].style.overflow = 'scroll'
+      pageHeader.classList.add('main-nav--closed');
+      pageHeader.classList.remove('main-nav--opened');
+      document.getElementsByTagName('body')[0].style.overflow = 'scroll';
     }
-  })
+  });
 
-  for (let headerLink of headerLinks) {
+  headerLinks.forEach(function (headerLink) {
     headerLink.addEventListener('click', function () {
-      pageHeader.classList.remove('main-nav--opened')
-      pageHeader.classList.add('main-nav--closed')
-      document.getElementsByTagName('body')[0].style.overflow = 'scroll'
-    })
-  }
-})()
+      pageHeader.classList.remove('main-nav--opened');
+      pageHeader.classList.add('main-nav--closed');
+      document.getElementsByTagName('body')[0].style.overflow = 'scroll';
+    });
+  });
+})();
