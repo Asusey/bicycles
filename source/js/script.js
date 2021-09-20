d231b2713dd6892f4dcf26c478cb83e8f75be4c9
Anna Tolstikova <39711434+Asusey@users.noreply.github.com>
2021-09-20 14:18:01 +0300

    Футер, переполнение, стайллинт

--- source/js/script.js
+++ source/js/script.js
@@ -1,26 +1,29 @@
 //"use strict";
 
 ;(function () {
-  var pageHeader = document.querySelector(".main-nav")
-  var headerToggle = document.querySelector(".main-nav__toggle")
-  var headerLinks = document.querySelectorAll(".main-nav__link")
+  var pageHeader = document.querySelector('.main-nav')
+  var headerToggle = document.querySelector('.main-nav__toggle')
+  var headerLinks = document.querySelectorAll('.main-nav__link')
 
-  pageHeader.classList.remove("main-nav--nojs")
+  pageHeader.classList.remove('main-nav--nojs')
 
-  headerToggle.addEventListener("click", function () {
-    if (pageHeader.classList.contains("main-nav--closed")) {
-      pageHeader.classList.remove("main-nav--closed")
-      pageHeader.classList.add("main-nav--opened")
+  headerToggle.addEventListener('click', function () {
+    if (pageHeader.classList.contains('main-nav--closed')) {
+      pageHeader.classList.remove('main-nav--closed')
+      pageHeader.classList.add('main-nav--opened')
+      document.getElementsByTagName('body')[0].style.overflow = 'hidden'
     } else {
-      pageHeader.classList.add("main-nav--closed")
-      pageHeader.classList.remove("main-nav--opened")
+      pageHeader.classList.add('main-nav--closed')
+      pageHeader.classList.remove('main-nav--opened')
+      document.getElementsByTagName('body')[0].style.overflow = 'scroll'
     }
   })
 
   for (let headerLink of headerLinks) {
-    headerLink.addEventListener("click", function () {
-      pageHeader.classList.remove("main-nav--opened")
-      pageHeader.classList.add("main-nav--closed")
+    headerLink.addEventListener('click', function () {
+      pageHeader.classList.remove('main-nav--opened')
+      pageHeader.classList.add('main-nav--closed')
+      document.getElementsByTagName('body')[0].style.overflow = 'scroll'
     })
   }
 })()
